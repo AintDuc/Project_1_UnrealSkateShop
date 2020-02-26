c5f59e8c1a11435a30a656a3b609c074531eeee8
Trinh Cao Van Duc <44729657+PanAngelosk@users.noreply.github.com>
2020-02-26 16:48:39 +0700

    Update film page

--- UnrealSkateShop/js/film.js
+++ UnrealSkateShop/js/film.js
@@ -2,8 +2,6 @@ var btnClose = document.getElementById("btn-close");
 var listItem = document.getElementsByClassName("item");
 var myOverlay = document.getElementById("overlay");
 
-
-
 function closeOldVideo(old) {
   return (old.style.display = "none");
 }
@@ -128,3 +126,151 @@ btnClose.addEventListener("click", function(e) {
 // //     // đồng thời hiển thị video đó to to ra
 // //   });
 // // });
+
+// responsive
+let device_height = window.screen.height;
+let device_width = window.screen.width;
+
+// let log = console.log;
+// myOverlay.id = "overlay_responsive";
+// log(myOverlay);
+
+if (device_width <= 576) {
+  myOverlay.id = "overlay_responsive";
+
+  Array.from(listItem).forEach(function(item) {
+    item.addEventListener("click", function(e) {
+      myOverlay.style.display = "initial";
+      if (Array.from(listItem).indexOf(item) === 0) {
+        myOverlay.childNodes[3].childNodes[1].style.display = "initial";
+      }
+      if (Array.from(listItem).indexOf(item) === 1) {
+        myOverlay.childNodes[3].childNodes[3].style.display = "initial";
+      }
+      if (Array.from(listItem).indexOf(item) === 2) {
+        myOverlay.childNodes[3].childNodes[5].style.display = "initial";
+      }
+      if (Array.from(listItem).indexOf(item) === 3) {
+        myOverlay.childNodes[3].childNodes[7].style.display = "initial";
+      }
+      if (Array.from(listItem).indexOf(item) === 4) {
+        myOverlay.childNodes[3].childNodes[9].style.display = "initial";
+      }
+      if (Array.from(listItem).indexOf(item) === 5) {
+        myOverlay.childNodes[3].childNodes[11].style.display = "initial";
+      }
+      if (Array.from(listItem).indexOf(item) === 6) {
+        myOverlay.childNodes[3].childNodes[13].style.display = "initial";
+      }
+      if (Array.from(listItem).indexOf(item) === 7) {
+        myOverlay.childNodes[3].childNodes[15].style.display = "initial";
+      }
+      if (Array.from(listItem).indexOf(item) === 8) {
+        myOverlay.childNodes[3].childNodes[17].style.display = "initial";
+      }
+    });
+  });
+
+  // khi ấn nút close chuyện gì xảy ra
+  btnClose.addEventListener("click", function(e) {
+    // cha nó đóng
+    this.parentNode.style.display = "none";
+    // tắt âm và ẩn video
+    myOverlay.childNodes[3].childNodes[1].muted = "true";
+    closeOldVideo(myOverlay.childNodes[3].childNodes[1]);
+
+    myOverlay.childNodes[3].childNodes[3].muted = "true";
+    closeOldVideo(myOverlay.childNodes[3].childNodes[3]);
+
+    myOverlay.childNodes[3].childNodes[5].muted = "true";
+    closeOldVideo(myOverlay.childNodes[3].childNodes[5]);
+
+    myOverlay.childNodes[3].childNodes[7].muted = "true";
+    closeOldVideo(myOverlay.childNodes[3].childNodes[7]);
+
+    myOverlay.childNodes[3].childNodes[9].muted = "true";
+    closeOldVideo(myOverlay.childNodes[3].childNodes[9]);
+
+    myOverlay.childNodes[3].childNodes[11].muted = "true";
+    closeOldVideo(myOverlay.childNodes[3].childNodes[11]);
+
+    myOverlay.childNodes[3].childNodes[13].muted = "true";
+    closeOldVideo(myOverlay.childNodes[3].childNodes[13]);
+
+    myOverlay.childNodes[3].childNodes[15].muted = "true";
+    closeOldVideo(myOverlay.childNodes[3].childNodes[15]);
+
+    myOverlay.childNodes[3].childNodes[17].muted = "true";
+    closeOldVideo(myOverlay.childNodes[3].childNodes[17]);
+  });
+}
+
+if (device_width <= 576 && device_width <= 768) {
+  myOverlay.id = "overlay_responsive";
+
+  Array.from(listItem).forEach(function(item) {
+    item.addEventListener("click", function(e) {
+      myOverlay.style.display = "initial";
+      if (Array.from(listItem).indexOf(item) === 0) {
+        myOverlay.childNodes[3].childNodes[1].style.display = "initial";
+      }
+      if (Array.from(listItem).indexOf(item) === 1) {
+        myOverlay.childNodes[3].childNodes[3].style.display = "initial";
+      }
+      if (Array.from(listItem).indexOf(item) === 2) {
+        myOverlay.childNodes[3].childNodes[5].style.display = "initial";
+      }
+      if (Array.from(listItem).indexOf(item) === 3) {
+        myOverlay.childNodes[3].childNodes[7].style.display = "initial";
+      }
+      if (Array.from(listItem).indexOf(item) === 4) {
+        myOverlay.childNodes[3].childNodes[9].style.display = "initial";
+      }
+      if (Array.from(listItem).indexOf(item) === 5) {
+        myOverlay.childNodes[3].childNodes[11].style.display = "initial";
+      }
+      if (Array.from(listItem).indexOf(item) === 6) {
+        myOverlay.childNodes[3].childNodes[13].style.display = "initial";
+      }
+      if (Array.from(listItem).indexOf(item) === 7) {
+        myOverlay.childNodes[3].childNodes[15].style.display = "initial";
+      }
+      if (Array.from(listItem).indexOf(item) === 8) {
+        myOverlay.childNodes[3].childNodes[17].style.display = "initial";
+      }
+    });
+  });
+
+  // khi ấn nút close chuyện gì xảy ra
+  btnClose.addEventListener("click", function(e) {
+    // cha nó đóng
+    this.parentNode.style.display = "none";
+    // tắt âm và ẩn video
+    myOverlay.childNodes[3].childNodes[1].muted = "true";
+    closeOldVideo(myOverlay.childNodes[3].childNodes[1]);
+
+    myOverlay.childNodes[3].childNodes[3].muted = "true";
+    closeOldVideo(myOverlay.childNodes[3].childNodes[3]);
+
+    myOverlay.childNodes[3].childNodes[5].muted = "true";
+    closeOldVideo(myOverlay.childNodes[3].childNodes[5]);
+
+    myOverlay.childNodes[3].childNodes[7].muted = "true";
+    closeOldVideo(myOverlay.childNodes[3].childNodes[7]);
+
+    myOverlay.childNodes[3].childNodes[9].muted = "true";
+    closeOldVideo(myOverlay.childNodes[3].childNodes[9]);
+
+    myOverlay.childNodes[3].childNodes[11].muted = "true";
+    closeOldVideo(myOverlay.childNodes[3].childNodes[11]);
+
+    myOverlay.childNodes[3].childNodes[13].muted = "true";
+    closeOldVideo(myOverlay.childNodes[3].childNodes[13]);
+
+    myOverlay.childNodes[3].childNodes[15].muted = "true";
+    closeOldVideo(myOverlay.childNodes[3].childNodes[15]);
+
+    myOverlay.childNodes[3].childNodes[17].muted = "true";
+    closeOldVideo(myOverlay.childNodes[3].childNodes[17]);
+  });
+}
